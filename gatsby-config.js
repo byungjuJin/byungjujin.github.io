1a54bb5ef51cb326dbe41f6c0da26ed2091af59c
billy <jinbju@gmail.com>
2021-07-26 23:04:48 +0900

    upgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          'UA-88866231-1'
+          siteConfig.googleAnalyticsId
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
@@ -30,14 +30,14 @@ module.exports = {
         //   cookie_expires: 0,
         // },
         // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ['/preview/**', '/do-not-track/me/too/'],
-        },
+        // pluginConfig: {
+        //   // Puts tracking script in the head instead of the body
+        //   head: false,
+        //   // Setting this parameter is also optional
+        //   respectDNT: true,
+        //   // Avoids sending pageview hits from custom paths
+        //   exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // },
       },
     },
     {
